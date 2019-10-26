9434839c79e8d6945f6679a61cdea10377473d6e
Gabriel Nepomuceno <gabriel.campos9815@gmail.com>
2019-10-26 01:13:44 -0300

    Revert "async get"
    This reverts commit 90d92fc1a77879b415879f3c8302704b9a6ba494.

--- assets/js/Services.js
+++ assets/js/Services.js
@@ -1,37 +1,51 @@
 var result = []
 
-// async function getInfo() {
-//     result = []
-//     for(let i=0;i<7;i++) {
-//         let xmlhttp = new XMLHttpRequest();
-//         xmlhttp.onreadystatechange = function() {
-//         if (this.readyState == 4 && this.status == 200) {
-//             result.push(JSON.parse(this.responseText));
-//         }
-//         };
-//         xmlhttp.open("GET", `https://hackaengine-dot-red-equinox-253000.appspot.com/sales?per_page=200&offset=${i*200}`, true);
-//         xmlhttp.send();
-//     }
-//     setTimeout(function(){
-//         console.log(result);
-//     }, 2000);
-// }
-
-const index = [0,1,2,3,4,5,6];
-var req = [
-    $.get('https://hackaengine-dot-red-equinox-253000.appspot.com/sales?per_page=200&offset=0'),
-    $.get('https://hackaengine-dot-red-equinox-253000.appspot.com/sales?per_page=200&offset=200'),
-    $.get('https://hackaengine-dot-red-equinox-253000.appspot.com/sales?per_page=200&offset=400'),
-    $.get('https://hackaengine-dot-red-equinox-253000.appspot.com/sales?per_page=200&offset=600'),
-    $.get('https://hackaengine-dot-red-equinox-253000.appspot.com/sales?per_page=200&offset=800'),
-    $.get('https://hackaengine-dot-red-equinox-253000.appspot.com/sales?per_page=200&offset=1000'),
-    $.get('https://hackaengine-dot-red-equinox-253000.appspot.com/sales?per_page=200&offset=1200')]
-
-async function getInfo(){
-    Promise.all(req).then(resp => {
-        console.log(req);
-    });
-    
+async function getInfo() {
+    result = []
+    for(let i=0;i<7;i++) {
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            result.push(JSON.parse(this.responseText));
+        }
+        };
+        xmlhttp.open("GET", `https://hackaengine-dot-red-equinox-253000.appspot.com/sales?per_page=200&offset=${i*200}`, true);
+        xmlhttp.send();
+    }
+    setTimeout(function(){
+        console.log(result);
+    }, 2000);
 }
 
 
+
+// const index = [0,1,2,3,4,5,6];
+// var data=[]
+
+
+// async function request(i) {
+//     try {
+//         value =await $.ajax({
+//             url: `https://hackaengine-dot-red-equinox-253000.appspot.com/sales?per_page=200&offset=${i*200}`,
+//             dataType: 'json',
+//             type: 'get',
+//             success: function(response){
+//                 console.log({STRING: response})
+//                 return response;
+//             },
+//             error: function(response){
+//                 console.log('erro'+response);
+//                 return "error";
+//             }
+//         });
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// async function getInfo(){
+//     // console.log({PONTO:ponto} )
+//     data  = index.map( async elem=>{
+//         return await request(elem);
+//     })
+// }
